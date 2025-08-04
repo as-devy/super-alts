fd02a5e92a25809b50756b11301bc22a1b912f21
as-devy <elbedwehykhaled@gmail.com>
2025-08-04 15:41:48 +0300

    commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../styles/style.css';
 import '../styles/layout/header.css';
 import '../styles/layout/footer.css';
