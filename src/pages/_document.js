f44decfa4c58edea0ec16fad60ef84482e6b83d9
as-devy <elbedwehykhaled@gmail.com>
2025-08-04 15:46:31 +0300

    commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,13 +4,13 @@ export default function Document() {
   return (
     <Html lang="en" dir="rtl">
       <Head>
-        {/* <link
+        <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
           integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
-        /> */}
+        />
         <link rel="shortcut icon" href="/assets/favicons/favicon.ico" />
         <link rel="apple-touch-icon" sizes="144x144" href="/assets/favicons/favicon-144.png" />
         <link rel="apple-touch-icon" sizes="168x168" href="/assets/favicons/favicon-168.png" />
