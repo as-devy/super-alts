b8dfeb0bad96a4520066e0d1b365264a6db68f48
as-devy <elbedwehykhaled@gmail.com>
2025-08-04 16:08:53 +0300

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,6 @@
-const isDev = process.env.NODE_ENV === 'development';
-
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' https://www.paypal.com https://cdn.jsdelivr.net 'unsafe-inline' 'unsafe-eval';
-  style-src 'self' https://fonts.googleapis.com https://cdnjs.cloudflare.com ${isDev ? "'unsafe-inline'" : ''};
-  style-src-elem 'self' https://fonts.googleapis.com https://cdnjs.cloudflare.com ${isDev ? "'unsafe-inline'" : ''};
-  font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com;
-  img-src 'self' data: https:;
-  connect-src 'self' https://www.paypal.com https://www.sandbox.paypal.com;
-  frame-src 'self' https://www.paypal.com https://www.sandbox.paypal.com;
-`;
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
