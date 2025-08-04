3407ae30481ff5d83fb6dfd9440aaf4460255019
as-devy <elbedwehykhaled@gmail.com>
2025-08-04 15:55:38 +0300

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' https://www.paypal.com https://cdn.jsdelivr.net 'unsafe-inline' 'unsafe-eval';
+  style-src 'self' https://fonts.googleapis.com https://cdnjs.cloudflare.com 'unsafe-inline';
+  style-src-elem 'self' https://fonts.googleapis.com https://cdnjs.cloudflare.com;
+  font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com;
+  img-src 'self' data: https:;
+  connect-src 'self' https://www.paypal.com;
+  frame-src 'self' https://www.paypal.com;
+`;
+
 const nextConfig = {
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
